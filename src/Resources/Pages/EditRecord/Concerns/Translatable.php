7287a35b7c7dc750cb75989cac0bc1231bad9c15
Ash Monsh <wh7r.com@gmail.com>
2025-04-30 16:22:22 +0300

    check if using the correct trait for pages

--- src/Resources/Pages/EditRecord/Concerns/Translatable.php
+++ src/Resources/Pages/EditRecord/Concerns/Translatable.php
@@ -2,12 +2,15 @@
 
 namespace LaraZeus\SpatieTranslatable\Resources\Pages\EditRecord\Concerns;
 
+use Filament\Resources\Pages\EditRecord;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Illuminate\Validation\ValidationException;
 use LaraZeus\SpatieTranslatable\Resources\Concerns\HasActiveLocaleSwitcher;
 use LaraZeus\SpatieTranslatable\Resources\Pages\Concerns\HasTranslatableFormWithExistingRecordData;
 use LaraZeus\SpatieTranslatable\Resources\Pages\Concerns\HasTranslatableRecord;
+use RuntimeException;
+use Throwable;
 
 trait Translatable
 {
@@ -17,6 +20,17 @@ trait Translatable
 
     protected ?string $oldActiveLocale = null;
 
+    /**
+     * @throws Throwable
+     */
+    public function bootTranslatable(): void
+    {
+        throw_unless(
+            is_subclass_of(static::class, EditRecord::class),
+            new RuntimeException('dont use the trait "' . Translatable::class . '" with "' . static::class . '"')
+        );
+    }
+
     public function getTranslatableLocales(): array
     {
         return static::getResource()::getTranslatableLocales();
@@ -25,7 +39,6 @@ trait Translatable
     protected function handleRecordUpdate(Model $record, array $data): Model
     {
         $translatableAttributes = static::getResource()::getTranslatableAttributes();
-
         $record->fill(
             Arr::except($data, $translatableAttributes)
         );
