2659dbb2670e1a18c19a66f81220e10b4b493eb5
Ash Monsh <wh7r.com@gmail.com>
2025-10-06 19:24:48 +0300

    fix local switcher

--- src/Resources/Pages/ViewRecord/Concerns/Translatable.php
+++ src/Resources/Pages/ViewRecord/Concerns/Translatable.php
@@ -16,7 +16,6 @@ trait Translatable
     use HasTranslatableFormWithExistingRecordData;
     use HasTranslatableRecord;
 
-    protected ?string $oldActiveLocale = null;
 
     /**
      * @throws Throwable
